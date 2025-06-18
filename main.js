1a0510b7b667455f808d0d17c8c011eefe2ed794
arvinr16 <arviansyahyk@gmail.com>
2025-06-18 19:57:47 +0700

    Adding 'media query max-width: 700px'

--- main.js
+++ main.js
@@ -49,19 +49,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
   mediaQuery.addEventListener("change", handleResize);
 });
-
-// JS jQuery
-// function toggleDetail(e) {
-//     const target = $(e.target);
-
-//     if($(target).hasClass("active")) {
-//         $(target).html("More Info").removeClass("active")
-//     } else {
-//         $(target).html("Less Info").addClass("active")
-//     }
-
-//     const item = $(target).parents(".about-exp-item")
-//     const detail = $(item).children(".about-exp-item-detail")
-
-//     $(detail).slideToggle();
-// }
